67dcd87c84414a6aaaa40e2dca694801b88abd70
keyiflerolsun <keyiflerolsun@gmail.com>
2024-08-07 17:40:36 +0300

    🕊
    🕊

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from io         import open
 setup(
     # ? Genel Bilgiler
     name         = "KeeneticPy",
-    version      = "1.9",
+    version      = "2.0",
     url          = "https://github.com/keyiflerolsun/KeeneticPy",
     description  = "Python Lib for Keenetic Routers",
     keywords     = ["KeeneticPy", "KekikAkademi", "keyiflerolsun"],
